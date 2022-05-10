0360fd6ae28dfc63967a92bacf7fed8335e31bfd
Aprochlometh <Aprochlometh@yahoo.com>
2022-05-10 11:47:22 -0600

    added exercise files

---
+++ outer_space.sql
@@ -0,0 +1,54 @@
+-- from the terminal run:
+-- psql < outer_space.sql
+
+DROP DATABASE IF EXISTS outer_space;
+
+CREATE DATABASE outer_space;
+
+\c outer_space
+
+CREATE TABLE planets
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+  orbits_around TEXT NOT NULL,
+  galaxy TEXT NOT NULL,
+  moons TEXT[]
+);
+
+INSERT INTO planets
+  (name, orbital_period_in_years, orbitted_bodies_id, galaxy_id, moons)
+VALUES
+  ('Earth', 1.00, 1, 1, '{"The Moon"}'),
+  ('Mars', 1.88, 1, 1, '{"Phobos", "Deimos"}'),
+  ('Venus', 0.62, 1, 1, '{}'),
+  ('Neptune', 164.8, 1, 1, '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+  ('Proxima Centauri b', 0.03, 2, 1, '{}'),
+  ('Gliese 876 b', 0.23, 3, 1, '{}');
+
+
+  CREATE TABLE galaxies
+  (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+  );
+
+INSERT INTO galaxies 
+(name)
+VALUES
+('Milky Way');
+
+
+  CREATE TABLE orbitted_bodies 
+  (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+  );
+
+  INSERT INTO orbitted_bodies
+  (name)
+  VALUES
+  ('The Sun'),
+  ('Proxima Centauri'),
+  ('Gliese 876');
